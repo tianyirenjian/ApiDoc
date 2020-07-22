6ff586293a84fae5f7b11adc0124dee2808672a0
tianyirenjian <179063828@qq.com>
2020-07-22 23:46:37 +0800

    初始化

---
+++ src/test/java/com/tianyisoft/apidoc/ApiDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianyisoft.apidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
